3fce5ee65dbc346a950435885ae8f3212d9578b3
ThainazZ <thainasd@hotmail.com>
2023-07-29 21:38:52 -0300

    fixing the link to whatsapp

--- src/FloatingWhatsAppButton.tsx
+++ src/FloatingWhatsAppButton.tsx
@@ -7,7 +7,7 @@ interface FloatingWhatsAppButtonProps {
 const FloatingWhatsAppButton: React.FC<FloatingWhatsAppButtonProps> = ({ whatsappLink }) => {
   return (
     <div style={{ position: 'fixed', bottom: '20px', right: '20px' }}>
-      <a href={whatsappLink} target="_blank" rel="noopener noreferrer">
+      <a href="https://api.whatsapp.com/send?phone=5588993730036" target="_blank" rel="noopener noreferrer">
         <img src="/assets/whatsapp.png" alt="WhatsApp" style={{ width: '50px', height: '50px' }} />
       </a>
     </div>
